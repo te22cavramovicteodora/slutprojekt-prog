2ac07fd61711c26e29647852793b9d6f6961e641
Teodora Avramovic <teodora.avramovic@elev.ga.ntig.se>
2024-03-18 09:31:03 +0100

    teodora avramovic slutprojekt sen commit hehehe

--- slutprojekt prr01/Program.cs
+++ slutprojekt prr01/Program.cs
@@ -1,1 +1,130 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Numerics;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "VERAS HOUSE OF HORRORS");
+Raylib.SetTargetFPS(60);
+
+string currentRoom = "start";
+ Rectangle amira = new Rectangle(320, 140, 140, 30);
+
+while (!Raylib.WindowShouldClose())
+{
+    if (currentRoom == "start")
+    {
+       // Vector2 amira = new Vector2(320, 140);
+        Vector2 mousePos = Raylib.GetMousePosition();
+
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.Black);
+       
+        Raylib.DrawText("VÄLKOMMEN TILL", 245, 40, 30, Color.Red);
+        Raylib.DrawText("VERAS HOUSE OF HORRORS", 180, 70, 30, Color.Red);
+        Raylib.DrawRectangleRec(amira, Color.Red);
+        Raylib.DrawText("TRYCK HÄR", 322 ,144, 23, Color.Black);
+        Raylib.LoadTextureFromImage();
+        Raylib.EndDrawing();
+
+        // Console.WriteLine(Raylib.IsMouseButtonPressed(MouseButton.Left) == true);
+
+         if (Raylib.CheckCollisionPointRec(mousePos, amira))
+         {
+            System.Console.WriteLine("Nuddar knappen");
+            if (Raylib.IsMouseButtonPressed(MouseButton.Left) == true)
+            {
+              currentRoom = "menu";
+            }
+         }
+
+    }
+    else if (currentRoom == "menu")
+    {
+        Vector2 firstButtonPosition = new Vector2(530, 160);
+        Vector2 secondButtonPosition = new Vector2 (530, 270);
+        Vector2 thirdButtonPosition = new Vector2(530, 380);
+        Vector2 mousePos = Raylib.GetMousePosition();
+
+
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.Black);
+        Raylib.DrawRectangle(225, 110, 350, 100, Color.Red);
+        Raylib.DrawRectangle(225, 220, 350, 100, Color.Red);
+        Raylib.DrawRectangle(225, 330, 350, 100, Color.Red);
+        Raylib.DrawCircleV(firstButtonPosition, 30, Color.Black);
+        Raylib.DrawCircleV(secondButtonPosition, 30, Color.Black);
+        Raylib.DrawCircleV(thirdButtonPosition, 30, Color.Black);
+        Raylib.DrawCircleV(firstButtonPosition, 25, Color.Red);
+        Raylib.DrawCircleV(secondButtonPosition, 25, Color.Red);
+        Raylib.DrawCircleV(thirdButtonPosition, 25, Color.Red);
+        Raylib.DrawText("i", 529, 147, 33, Color.Black);
+        Raylib.DrawText("i", 529, 257, 33, Color.Black);
+        Raylib.DrawText("i", 529, 367, 33, Color.Black);
+        Raylib.DrawText("1.Veras spegel labyrint", 245, 149, 20, Color.Black);
+        Raylib.DrawText("2.Veras tetris extravaganza", 245, 259, 18, Color.Black);
+        Raylib.DrawText("3.Veras easteregg hunt", 245, 369, 20, Color.Black);
+        Raylib.DrawText("Skriv numret av det minispelet du vill välja", 5, 580, 20, Color.Red);
+
+        if (Raylib.CheckCollisionPointCircle(mousePos, firstButtonPosition, 30))
+        {
+        Raylib.DrawRectangle(5, 5, 790, 100, Color.Red);
+        Raylib.DrawText("Veras spegellabyrint är ett labyrintspel helt utan jumpscares och easter-\neggs jag lovar. Målet med banan är att klara labyrinten.", 10, 10, 20, Color.Black );
+        }
+        if (Raylib.CheckCollisionPointCircle(mousePos, secondButtonPosition, 30))
+        {
+        Raylib.DrawRectangle(5, 5, 790, 100, Color.Red);
+        Raylib.DrawText("Veras tetris extravaganza talar för sig själv. Vera älskar tetris och det\n gör alla andra på jorden också.", 10, 10, 20, Color.Black );
+        }
+         if (Raylib.CheckCollisionPointCircle(mousePos, thirdButtonPosition, 30))
+        {
+        Raylib.DrawRectangle(5, 5, 790, 100, Color.Red);
+        Raylib.DrawText("Det svåraste, sorgligaste, mest utmärkta, ryggradskittlande, tår tårkande,\nmest faantastiska, uttänkta, A-i-slutbetyg-förtjänande, underbara,smarta,\n gjort av ett geni- spelet du någonsin kommer köra,synd bara att det inte\n finns än.", 10, 10, 20, Color.Black );
+        }
+        Raylib.EndDrawing();
+    }
+    else if (currentRoom == "labyrint")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.One))
+        {
+        currentRoom = "labyrint";
+        }
+
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.Black);
+        Raylib.DrawRectangle(10, 10, 10, 10, Color.Blue);
+        Raylib.EndDrawing();
+    }
+}
+
+
+
+
+
+// choice = Console.ReadLine();
+//choice = choice.ToLower();
+
+// if (choice == "1")
+// {
+//     Console.WriteLine("hehe");
+// }
+
+// if (choice == "info-1")
+// {
+//     Console.WriteLine("Veras spegellabyrint är ett labyrintspel helt utan jumpscares och eastereggs jag lovar. Målet med banan är att klara labyrinten.");
+// }
+
+//if (choice == "info-2")
+// {
+//     Console.WriteLine("Veras tetris extravaganza talar för sig själv. Vera älskar tetris och det gör alla andra på gjorden också.");
+// }
+// if (choice == "info-3")
+// {
+//     Console.WriteLine("Det svåraste, sorgligaste, mest utmärkta, ryggradskittlande, tår tårkande, mest faantastiska, uttänkta, A-i-slutbetyg-förtjänande, underbara, smarta, gjort av ett geni- spelet du någonsin kommer köra, synd bara att det inte finns än.");
+// }
+
+//Console.ReadLine();
+
+
+
+
+
--- slutprojekt prr01/obj/Debug/net8.0/slutprojekt prr01.AssemblyInfo.cs
+++ slutprojekt prr01/obj/Debug/net8.0/slutprojekt prr01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("slutprojekt prr01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa541f96d077ff73f1d15e8a1fb4445b917d936")]
 [assembly: System.Reflection.AssemblyProductAttribute("slutprojekt prr01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("slutprojekt prr01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
